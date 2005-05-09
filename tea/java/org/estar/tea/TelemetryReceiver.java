36417a67d04cfb66ae0ec5d4536260d8bfaf82df
Chris Mottram <C.Mottram@ljmu.ac.uk>
2005-05-09 13:45:29 +0000

    Initial revision

---
+++ tea/java/org/estar/tea/TelemetryReceiver.java
@@ -0,0 +1,51 @@
+package org.estar.tea;
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+//import javax.net.ssl.*;
+//import javax.security.cert.*;
+import java.util.*;
+import java.text.*;
+
+
+import org.estar.astrometry.*;
+import org.estar.rtml.*;
+import org.estar.io.*;
+
+import ngat.util.*;
+import ngat.util.logging.*;
+import ngat.net.*;
+import ngat.net.camp.*;
+import ngat.astrometry.*;
+
+import ngat.message.GUI_RCS.*;
+
+public class TelemetryReceiver extends CAMPServer {
+
+    /** The EmbeddedAgent which this server is attached to.*/
+    TelescopeEmbeddedAgent tea;
+
+    public TelemetryReceiver(TelescopeEmbeddedAgent tea, String name) {
+	super(name);
+	this.tea = tea;
+	handlerFactory = new TelemetryHandlerFactory(tea);
+    }
+
+    public static final void main(String args[]) {
+	int port = 0;
+	try {
+	    port = Integer.parseInt(args[0]);
+	    TelemetryReceiver tr = new TelemetryReceiver(null, "TEST");
+	    tr.bind(port);
+	    tr.start();
+
+	    System.err.println("Started TelemetryReceiver on port: "+port);
+
+	} catch (Exception e) {
+	    System.err.println("Failed to start TelemetryReceiver on port: "+port);
+	}
+
+    }
+
+}
