2e1426b6fe0a63fa9fc8a1ef41f2853e1b51bc93
Steve Fraser <snf@example.com>
2005-05-27 13:25:05 +0000

    Modified to access ARQ handling each document.

--- tea/java/org/estar/tea/DocumentExpirator.java
+++ tea/java/org/estar/tea/DocumentExpirator.java
@@ -26,130 +26,134 @@ import ngat.message.OSS.*;
  * is after now + some constant. These documents will never be done and are expired in the database,
  * so we create a failed document and send it to the agent.
  */
-public class DocumentExpirator extends ControlThread implements Logging
-{
-	/**
-	 * Class constant for logging.
-	 */
-	public static final String CLASS = "DocumentExpirator";
+public class DocumentExpirator extends ControlThread implements Logging {
 
-	/** Polling time.*/
-	long time;
+    /**
+     * Class constant for logging.
+     */
+    public static final String CLASS = "DocumentExpirator";
+    
+    /** Polling time.*/
+    long time;
+    
+    /** Offset to allow after expiry.*/
+    long offset;
+    
+    TelescopeEmbeddedAgent tea;
+    
+    /**
+     * Logger.
+     */
+    Logger logger = null;
+    
+    /**
+     * Constructor. Also creates logger.
+     * @param tea The instance of TelescopeEmbeddedAgent.
+     * @param time How often the expirator runs.
+     * @param offset How long after now the document expirity date is before we send a message to the agent.
+     *        This ensures a currently running document is not expired.
+     * @see #tea
+     * @see #time
+     * @see #offset
+     * @see #logger
+     */
+    public DocumentExpirator(TelescopeEmbeddedAgent tea, long time, long offset)
+    {
+	super("DOCEXP", true);
+	this.tea    = tea;
+	this.time   = time;
+	this.offset = offset;
+	logger = LogManager.getLogger(this);
+    }
 
-	/** Offset to allow after expiry.*/
-	long offset;
-
-	TelescopeEmbeddedAgent tea;
-
-	/**
-	 * Logger.
-	 */
-	Logger logger = null;
-
-	/**
-	 * Constructor. Also creates logger.
-	 * @param tea The instance of TelescopeEmbeddedAgent.
-	 * @param time How often the expirator runs.
-	 * @param offset How long after now the document expirity date is before we send a message to the agent.
-	 *        This ensures a currently running document is not expired.
-	 * @see #tea
-	 * @see #time
-	 * @see #offset
-	 * @see #logger
-	 */
-	public DocumentExpirator(TelescopeEmbeddedAgent tea, long time, long offset)
-	{
-		super("DOCEXP", true);
-		this.tea    = tea;
-		this.time   = time;
-		this.offset = offset;
-		logger = LogManager.getLogger(this);
-	}
-
-	protected void initialise() {}
+    protected void initialise() {}
     
     
-	protected void mainTask()
-	{	
-		try
-		{
-			logger.log(INFO,1,CLASS,tea.getId(),"mainTask","Running expirator.");
-			// Check to see if there are any expired docs still kept by the TEA.
-
-			RTMLDocument    doc   = null;
-			RTMLObservation obs   = null;
-			RTMLSchedule    sched = null;
-			RTMLIntelligentAgent agent = null;
-			AgentRequestHandler arq = null;
-			String documentId = null;
-			String key = null;
-
-			List keyList = tea.getDocumentKeysList();
-			for(int i = 0; i< keyList.size(); i++)
-			{
-				key = (String)(keyList.get(i));
-				doc = tea.getDocument(key);
-				if(doc != null)
-				{
-					obs = doc.getObservation(0);
-					if (obs != null)
-					{
-						sched = obs.getSchedule();
-						if (sched != null)
-						{
-							Date endDate = sched.getEndDate();
-							if (endDate != null)
-							{
-								long end = endDate.getTime();
-								logger.log(INFO,1,CLASS,tea.getId(),"mainTask",
-									   "Expirator testing document "+key+
-									   " with end date "+endDate+".");
-								if (end < (System.currentTimeMillis()-offset))
-								{
-									// Expire the Doc - simple for now needs FG/MG stuff
-									agent = doc.getIntelligentAgent();
-									if(agent != null)
-										documentId = agent.getId();
-									logger.log(INFO,1,CLASS,tea.getId(),"mainTask",
-										   "Expirator trying to expire document "+documentId+".");
-									try
-									{
-										// send failed/incomplete to UA
-										arq = new AgentRequestHandler(tea);
-										if(obs.getImageDataCount() > 0)
-											arq.sendDocUpdate(doc,"incomplete");
-										else
-											arq.sendDocUpdate(doc,"failed");
-										// save document
-										tea.saveDocument(key,doc);
-										// move document to expired dir
-										// delete document from memory
-										tea.deleteDocument(key);
-									}
-									catch (Exception e)
-									{
-										logger.log(INFO,1,CLASS,tea.getId(),"mainTask",
-											   "Expirator failed for document "+
-											   documentId+":"+e);
-										logger.dumpStack(1,e);
-									}
-								}
-							}
-						}
+    protected void mainTask() {	
+	try {
+	    logger.log(INFO,1,CLASS,tea.getId(),"mainTask","Running expirator.");
+	    // Check to see if there are any expired docs still kept by the TEA.
+	    
+	    RTMLDocument    doc   = null;
+	    RTMLObservation obs   = null;
+	    RTMLSchedule    sched = null;
+	    RTMLIntelligentAgent agent = null;
+	    AgentRequestHandler arq = null;
+	    String documentId = null;
+	    String key = null;
+	    
+	    // Note this is now a list of agents
+	    List keyList = tea.getDocumentKeysList();
+	    for(int i = 0; i< keyList.size(); i++) {
+		
+		key = (String)(keyList.get(i));
+		arq = tea.getUpdateHandler(key);
+		
+		if (arq != null) {
+		    
+		    doc = arq.getBaseDocument();
+		
+		    if (doc != null) {
+			
+			obs = doc.getObservation(0);
+			
+			if (obs != null) {
+			    
+			    sched = obs.getSchedule();
+			    
+			    if (sched != null) {
+				
+				Date endDate = sched.getEndDate();
+				
+				if (endDate != null) {
+				    
+				    long end = endDate.getTime();
+				    logger.log(INFO,1,CLASS,tea.getId(),"mainTask",
+					       "Expirator testing document "+key+
+					       " with end date "+endDate+".");
+				    if (end < (System.currentTimeMillis()-offset)) {
+					
+					// Expire the Doc - simple for now needs FG/MG stuff
+					agent = doc.getIntelligentAgent();
+					if(agent != null)
+					    documentId = agent.getId();
+					logger.log(INFO,1,CLASS,tea.getId(),"mainTask",
+						   "Expirator trying to expire document "+documentId+".");
+					try {
+					    // send failed/incomplete to UA
+					    
+					    if(obs.getImageDataCount() > 0)
+						arq.sendDocUpdate(doc,"incomplete");
+					    else
+						arq.sendDocUpdate(doc,"failed");
+	
+					    arq.expireDocument();
+				  
 					}
+					catch (Exception e) {
+					    logger.log(INFO,1,CLASS,tea.getId(),"mainTask",
+						       "Expirator failed for document "+
+						       documentId+":"+e);
+					    logger.dumpStack(1,e);
+					}
+				    }
 				}
+			    }
 			}
+		    }
 		}
-		catch(Exception e)
-		{
-			logger.log(INFO,1,CLASS,tea.getId(),"mainTask","Expirator caught un-caught exception "+e+".");
-			logger.dumpStack(1,e);
-		}
-		logger.log(INFO,1,CLASS,tea.getId(),"mainTask","Expirator sleeping for "+time+" ms.");
-		try {Thread.sleep(time);} catch (InterruptedException ix) {}
+	    }
 	}
+	catch(Exception e)
+	    {
+		logger.log(INFO,1,CLASS,tea.getId(),"mainTask","Expirator caught un-caught exception "+e+".");
+		logger.dumpStack(1,e);
+	    }
+	logger.log(INFO,1,CLASS,tea.getId(),"mainTask","Expirator sleeping for "+time+" ms.");
+	try {Thread.sleep(time);} catch (InterruptedException ix) {}
+    }
     
-	protected void shutdown() {}
+    protected void shutdown() {}
     
 
 }
