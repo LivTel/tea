63fd9e7a3d64d3cd00226f25c845cc3ae9f502d9
Steve Fraser <snf@example.com>
2007-04-04 08:55:52 +0000

    removed spurious loggers, using TRACE for all main loggers and bogstanlogformatter

--- tea/java/org/estar/tea/TelescopeEmbeddedAgent.java
+++ tea/java/org/estar/tea/TelescopeEmbeddedAgent.java
@@ -3,11 +3,12 @@ package org.estar.tea;
 import java.io.*;
 import java.util.*;
 import java.net.*;
-//import javax.net.ssl.*;
-//import javax.security.cert.*;
-import java.util.*;
 import java.text.*;
-
+import java.rmi.*;
+import javax.net.*;
+import javax.net.ssl.*;
+import java.security.*;
+import java.security.cert.*;
 
 import org.estar.astrometry.*;
 import org.estar.rtml.*;
@@ -31,10 +32,15 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 	/**
 	 * Revision control system version id.
 	 */
-	public final static String RCSID = "$Id: TelescopeEmbeddedAgent.java,v 1.23 2007-02-20 13:19:27 snf Exp $";
+	public final static String RCSID = "$Id: TelescopeEmbeddedAgent.java,v 1.24 2007-04-04 08:55:52 snf Exp $";
 
 	public static final String CLASS = "TelescopeEA";
     
+    public static final int ASYNC_MODE_SOCKET = 1;
+
+    public static final int ASYNC_MODE_RMI = 2;
+
+
 	/** Default agent name for logging.*/
 	public static final String DEFAULT_ID = "TELESCOPE_EA";
     
@@ -82,6 +88,12 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 	public static final String DEFAULT_SSL_TRUST_FILE_NAME = "nosuchtruststore.public";
     
 	public static final String DEFAULT_SSL_KEY_FILE_PASSWORD    = "secret";
+
+    	public static final String DEFAULT_OSS_KEY_FILE_NAME   = "nosuchkeystore.private";
+    
+	public static final String DEFAULT_OSS_TRUST_FILE_NAME = "nosuchtruststore.public";
+    
+	public static final String DEFAULT_OSS_KEY_FILE_PASSWORD    = "secret";
     
 	public static final int    DEFAULT_SSL_FILE_XFER_BANDWIDTH = 50; // kbyte/s
     
@@ -132,7 +144,11 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
     
 	/** True if the OSS requires a secure connection.*/
 	protected boolean ossConnectionSecure;
-    
+
+    	protected File   ossKeyFile;
+	protected File   ossTrustFile;
+	protected String ossPassword;
+
 	/** RCS CTRL Server port.*/
 	protected int ctrlPort;
     
@@ -237,7 +253,18 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 
 	/** Checks Documents for expiration.*/
 	protected DocumentExpirator docExpirator;
-   
+
+    protected AgentRequestHandlerMonitoringThread arqMonitor;
+
+    /** Which mode we make asynch responses.*/
+    protected int asyncResponseMode;
+
+    /** The host where we can lookup the NAR if RMI mode.*/
+    protected String narHost;
+
+    /** Predicts availability of telescope over various time frames.*/
+    protected TelescopeAvailabilityPredictor tap;
+
 	/** Create a TEA with the supplied ID.*/
 	public TelescopeEmbeddedAgent(String id) {
 
@@ -251,7 +278,7 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 
 		io = new eSTARIO();
 	
-		ConsoleLogHandler console = new ConsoleLogHandler(new BasicLogFormatter(150));
+		ConsoleLogHandler console = new ConsoleLogHandler(new BogstanLogFormatter());
 		console.setLogLevel(ALL);
 	
 		traceLog   = LogManager.getLogger("TRACE");
@@ -259,27 +286,18 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 		traceLog.addHandler(console);
 	
 		// add handler to other loggers used
-		l = LogManager.getLogger("org.estar.tea.TelescopeEmbeddedAgent");
-		l.setLogLevel(ALL);
-		l.addHandler(console);
+	
 		l = LogManager.getLogger("org.estar.tea.TelemetryRequestor");
 		l.setLogLevel(ALL);
 		l.addHandler(console);
 		l = LogManager.getLogger("org.estar.tea.TelemetryHandlerFactory");
 		l.setLogLevel(ALL);
 		l.addHandler(console);
-		l = LogManager.getLogger("org.estar.tea.TelemetryHandler");
-		l.setLogLevel(ALL);
-		l.addHandler(console);
-		l = LogManager.getLogger("org.estar.tea.UpdateHandler");
-		l.setLogLevel(ALL);
-		l.addHandler(console);
+	
 		l = LogManager.getLogger("org.estar.tea.DocumentExpirator");
 		l.setLogLevel(ALL);
 		l.addHandler(console);
-		l = LogManager.getLogger("org.estar.tea.AgentRequestHandler");
-		l.setLogLevel(ALL);
-		l.addHandler(console);
+	
 		l = LogManager.getLogger("org.estar.tea.DefaultPipelinePlugin");
 		l.setLogLevel(ALL);
 		l.addHandler(console);
@@ -325,7 +343,7 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 		docExpirator = new DocumentExpirator(this, expiratorSleepMs,
 						     DEFAULT_EXPIRATOR_OFFSET_TIME); 
 		traceLog.log(INFO, 1, CLASS, id, "init",
-			     "DocExpirator with polling interval: "+
+			     "Create DocExpirator with polling interval: "+
 			     (expiratorSleepMs/1000)+" secs");
 	
 		traceLog.log(INFO, 1, CLASS, id, "init", "Create SSL File Client...");
@@ -355,6 +373,13 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 				 "Image TransferRelay: "+relayHost+" : "+relayPort);		    
 		}
 
+		String asmode = "UNKNOWN";
+		if (asyncResponseMode == ASYNC_MODE_SOCKET)
+		    asmode = "SOCKET";
+		else
+		    asmode = "RMI: "+narHost;
+			
+
 		traceLog.log(INFO, 1, CLASS, id, "init",
 			     "Starting Telescope Embedded Agent: "+id+
 			     "\n Incoming port: "+dnPort+		     
@@ -362,7 +387,7 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 			     "\n    Root:       "+dbRootName+
 			     "\n    Connect:    "+(ossConnectionSecure ? " SECURE" : " NON_SECURE")+
 			     "\n  Ctrl:         "+ctrlHost+" : "+ctrlPort+
-			     "\n"+
+			     "\n  Async Mode:   "+asmode+
 			     "\n Telescope:"+
 			     "\n  Lat:          "+Position.toDegrees(siteLatitude, 3)+
 			     "\n  Long:         "+Position.toDegrees(siteLongitude,3)+
@@ -381,6 +406,36 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 			traceLog.dumpStack(1,e);
 		}
 
+                if (System.getProperty("registerHandler") != null) {
+                    traceLog.log(INFO, 1, CLASS, id, "init",
+                                 "Registering EA-RequestHandler...");
+                    try {
+                        DefaultEmbeddedAgentRequestHandler ear = new DefaultEmbeddedAgentRequestHandler(this);
+                        Naming.rebind("rmi://localhost/EARequestHandler", ear);
+                        traceLog.log(INFO, 1, CLASS, id, "init",
+                                     "EmbeddedAgentRequestHandler bound to registry");
+                    } catch (Exception e) {
+                        traceLog.log(INFO, 1, CLASS, id, "init",
+                                     "Failed to register EAR: "+e);
+                    }
+
+		    try {
+			 tap = new DefaultTelescopeAvailabilityPredictor();
+			 Naming.rebind("rmi://localhost/TAPredictor", tap);
+			 traceLog.log(INFO, 1, CLASS, id, "init",
+				      "TelescopeAvailabilityPredictor bound to registry");
+		    } catch (Exception e) {
+                        traceLog.log(INFO, 1, CLASS, id, "init",
+                                     "Failed to register TAP: "+e);
+                    }
+
+                }
+		
+		arqMonitor = new AgentRequestHandlerMonitoringThread(this);
+		arqMonitor.start();
+		traceLog.log(INFO, 1, CLASS, id, "init",
+			     "Started ARQ Monitoring");
+		
 		telemetryServer.start();	
 		traceLog.log(INFO, 1, CLASS, id, "init",
 			     "Started Telemetry Server on port: "+telemetryPort+
@@ -399,9 +454,6 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 		io.serverStart(dnPort, this);	
 		traceLog.log(INFO, 1, CLASS, id, "init",
 			     "Started eSTAR IO server.");
-	
-		traceLog.log(INFO, 1, CLASS, id, "init",
-			     "TEA: "+id+" is up and running");
 
 	}
     
@@ -446,6 +498,17 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 	/** Sets whether the OSS Connection is secure.*/    
 	public void setOssConnectionSecure(boolean s) { this.ossConnectionSecure = s; }
 
+
+	/** Sets the SSL transfer keystore.*/
+	public void setOssKeyFile(File k) { ossKeyFile = k; }
+    
+	/** Sets the SSL transfer trust store.*/
+	public void setOssTrustFile(File t) { ossTrustFile = t; };
+    
+	/** Sets the SSL transfer keypass.*/
+	public void setOssPassword(String p) { ossPassword = p; }
+    
+
 	/** Sets the RCS CTRL port.*/
 	public void setCtrlPort(int p) { this.ctrlPort = p; }
     
@@ -466,7 +529,12 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
     
 	/** Sets the Base dir where transferred images are placed. ###project-specific*/
 	public void setImageDir(String d) { this.imageDir = d; }
-    
+
+    /** Set the NodeAgent Asynch Response Handler host address.*/
+    public void setNarHost(String h) { this.narHost = h; }
+
+    public void setAsyncResponseMode(int m) { this.asyncResponseMode = m; }
+
 	/** Sets the site latitude (rads).*/
 	public void setSiteLatitude(double l) { this.siteLatitude = l; }
     
@@ -718,6 +786,11 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 		agentMap.put(oid, arq);
 	}
     
+    /** Returms the Agent Map.*/
+    public Map getAgentMap() {
+	return agentMap;
+    }
+
 	/** Find an ARQ for observationID.
 	 * @param oid The observationID.
 	 * @return The ARQ for the specified observationID.
@@ -1035,37 +1108,62 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 	 * @param document The document to send.
 	 * @param type     A string denoting the type of document to send.
 	 */
-	public void sendDocumentToIA(RTMLDocument document) throws Exception
-	{
-		traceLog.log(INFO, 1,CLASS, "sendDocumentToIA Started.");
+	public void sendDocumentToIA(RTMLDocument document) throws Exception {
+		
+	    traceLog.log(INFO, 1,CLASS, "sendDocumentToIA Started.");
+
+	    switch (asyncResponseMode) {
+	    case ASYNC_MODE_SOCKET:
+		
 		RTMLIntelligentAgent userAgent = document.getIntelligentAgent();
-		if(userAgent == null)
-		{
-			traceLog.log(INFO, 1, CLASS, "sendDocumentToIA: User agent was null.");
-			throw new Exception(this.getClass().getName()+":sendDocumentToIA:user agent was null");
+		if(userAgent == null) {
+		    traceLog.log(INFO, 1, CLASS, "sendDocumentToIA: User agent was null.");
+		    throw new Exception(this.getClass().getName()+":sendDocumentToIA:user agent was null");
 		}
 		String agid = userAgent.getId();
 		String host = userAgent.getHostname();
 		int    port = userAgent.getPort();
-	
+		
 		traceLog.log(INFO, 1, CLASS, "sendDocumentToIA: Opening eSTAR IO client connection to ("+host+
-			   ","+port+").");
+			     ","+port+").");
 		GlobusIOHandle handle = io.clientOpen(host, port);
-		if(handle == null)
-		{
-			traceLog.log(INFO, 1, CLASS, "sendDocumentToIA:Failed to open client connection to ("+host+
-				   ","+port+").");
-			throw new Exception(this.getClass().getName()+":sendDocumentToIA:handle was null");
+		if(handle == null) {
+		    traceLog.log(INFO, 1, CLASS, "sendDocumentToIA:Failed to open client connection to ("+host+
+				 ","+port+").");
+		    throw new Exception(this.getClass().getName()+":sendDocumentToIA:handle was null");
 		}
 		String reply = TelescopeEmbeddedAgent.createReply(document);
-	
+		
 		traceLog.log(INFO, 1, CLASS, "sendDocumentToIA:Writing:...\n"+reply+"\n ...to handle "+handle+".");
 		// how do we check this has failed?
 		io.messageWrite(handle, reply);
 		traceLog.log(INFO, 1, CLASS,"sendDocumentToIA:Sent document "+agid+".");
 		io.clientClose(handle);
-	}
+		
+		break;
 
+	    case ASYNC_MODE_RMI:
+
+		NodeAgentAsynchronousResponseHandler narh = (NodeAgentAsynchronousResponseHandler)Naming.
+		    lookup("rmi://"+narHost+"/NAAsyncResponseHandler");
+ 
+		if (narh == null) {
+		    traceLog.log(INFO, 1, CLASS, "sendDocumentToIA: NA ResponseHandler was null.");
+		    throw new Exception(this.getClass().getName()+":sendDocumentToIA:Lookup [NAAsyncResponseHandler] was null.");
+		}
+
+		narh.handleAsyncResponse(document);
+
+		traceLog.log(INFO, 1, CLASS,"sendDocumentToIA:Sent document successfully via NA ResponseHandler");
+
+		break;
+
+	    default:
+
+	    }
+		
+	}
+	
 
 	/** Configures and starts a Telescope Embedded Agent.*/
 	public static void main(String args[]) {
@@ -1077,6 +1175,15 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 			System.exit(1);
 		}
 	
+		try {
+		    if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new RMISecurityManager());
+		    }
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    return;
+		}
+
 		String id = args[0];
 
 		File configFile = new File(args[1]);
@@ -1130,6 +1237,14 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 
 		boolean ossSecure = (config.getProperty("oss.secure") != null);
 	
+		String okf   = config.getProperty("oss.keyfile", DEFAULT_OSS_KEY_FILE_NAME);
+		File ossKeyFile = new File(okf);
+		
+		String otf   = config.getProperty("oss.trustfile", DEFAULT_OSS_TRUST_FILE_NAME);
+		File ossTrustFile = new File(otf);
+		
+		String osspass = config.getProperty("oss.pass",   DEFAULT_OSS_KEY_FILE_PASSWORD);
+	
 		int ctrlPort = config.getIntValue("ctrl.port", DEFAULT_CTRL_PORT);
 	
 		String ctrlHost = config.getProperty("ctrl.host", DEFAULT_CTRL_HOST);
@@ -1178,11 +1293,26 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 		String expiredDocumentDirectory = config.getProperty("expired.document.dir",
 								     DEFAULT_EXPIRED_DOCUMENT_DIR);
 		long expiratorSleepMs = config.getLongValue("expirator.sleep", DEFAULT_EXPIRATOR_POLLING_TIME);
-	
+
+		String arm = config.getProperty("async.response.mode", "SOCKET");
+
+		if ("RMI".equals(arm)) {		
+		    String nh = config.getProperty("async.response.handler.host", "localhost");
+		    setAsyncResponseMode(ASYNC_MODE_RMI);
+		    setNarHost(nh);
+		} else if
+		    ("SOCKET".equals(arm)){
+		    setAsyncResponseMode(ASYNC_MODE_SOCKET);	
+		} else
+		    throw new IllegalArgumentException("Unknown response handler mode; "+arm);
+		
 		setDnPort(dnPort);
 		setOssHost(ossHost);
 		setOssPort(ossPort);
 		setOssConnectionSecure(ossSecure);
+		setOssKeyFile(ossKeyFile);
+		setOssTrustFile(ossTrustFile);
+		setOssPassword(osspass);
 		setCtrlHost(ctrlHost);
 		setCtrlPort(ctrlPort);
 		setTelemetryHost(telHost);
@@ -1221,8 +1351,10 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
 	/** Prints a message and usage instructions.*/
 	private static void usage(String message) {
 		System.err.println("TEA initialization: "+message);
-		System.err.println("Usage: java -Djava.security.egd=<entropy-gathering-url> -Dastrometry.impl=<astrometry-class> \\"+
-				   "\n          org.estar.tea.TelescopeEmbeddedAgent <id> <config-file> "); 
+		System.err.println("Usage: java -Djava.security.egd=<entropy-gathering-url> "+
+				   "\n -Dastrometry.impl=<astrometry-class> \\"+
+				   "\n [-DregisterHandler=true] "+
+				   "\n org.estar.tea.TelescopeEmbeddedAgent <id> <config-file> "); 
 	
 	}
     
@@ -1239,23 +1371,72 @@ public class TelescopeEmbeddedAgent implements eSTARIOConnectionListener, Loggin
     
 	private class TEAConnectionFactory implements ConnectionFactory {
 	
-		public IConnection createConnection(String connId) throws UnknownResourceException {
+	    public IConnection createConnection(String connId) throws UnknownResourceException {
 	    
 			if (connId.equals("CTRL")) {
 				return new SocketConnection(ctrlHost, ctrlPort);
 			} else if
 				(connId.equals("OSS")) {
 				return new SocketConnection(ossHost, ossPort);
+			} else if
+			    (connId.equals("OSS_SECURE")) {
+
+			    try {
+				KeyManager[]   kms = getKeyManagers(ossKeyFile, ossPassword);
+				TrustManager[] tms = getTrustManagers(ossTrustFile);
+				
+				SSLContext context = SSLContext.getInstance("TLS");
+				
+				context.init(kms, tms, null);
+				
+				SSLSocketFactory secureSocketFactory = (SSLSocketFactory)context.getSocketFactory();
+				
+				return new SocketConnection(ossHost, ossPort, secureSocketFactory );
+				
+			    } catch (Exception e) {
+				e.printStackTrace();
+				throw new UnknownResourceException("Unable to create secure connection: "+e);
+			    }
 			} else
-				throw new UnknownResourceException("Not known: "+connId);
+			    throw new UnknownResourceException("Not known: "+connId);
 		}
 	
+	    private KeyManager[] getKeyManagers(File keyStore, String password) throws Exception {
+            String alg = KeyManagerFactory.getDefaultAlgorithm();
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(alg);
+            FileInputStream fis = new FileInputStream(keyStore);
+            KeyStore ks = KeyStore.getInstance("jks");
+            ks.load(fis, password.toCharArray());
+            fis.close();
+            kmf.init(ks, password.toCharArray());
+            KeyManager[] kms = kmf.getKeyManagers();
+            return kms;
+        }
+
+        private TrustManager[] getTrustManagers(File trustStore) throws Exception {
+
+            String alg = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(alg);
+            FileInputStream fis = new FileInputStream(trustStore);
+            KeyStore ks = KeyStore.getInstance("jks");
+            ks.load(fis, "public".toCharArray());
+            fis.close();
+            tmf.init(ks);
+            TrustManager[] tms = tmf.getTrustManagers();
+            return tms;
+        }
+
+
+
 	}
     
 }
 
 /* 
 ** $Log: not supported by cvs2svn $
+** Revision 1.23  2007/02/20 13:19:27  snf
+** added a stack dump to tea createErrorDoc if the error cannot be created - weird
+**
 ** Revision 1.22  2006/12/11 11:49:13  snf
 ** Added a test for rename doc failure in expireDoc soas to throw an Exception.
 **
