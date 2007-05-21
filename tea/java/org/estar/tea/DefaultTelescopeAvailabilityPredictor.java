9b99fa034b6c9151fabc7bb63a7b5388eafc38a8
Steve Fraser <snf@example.com>
2007-05-21 13:30:49 +0000

    added persistance storing and extra file parameter to constructor.

--- tea/java/org/estar/tea/DefaultTelescopeAvailabilityPredictor.java
+++ tea/java/org/estar/tea/DefaultTelescopeAvailabilityPredictor.java
@@ -1,15 +1,32 @@
 package org.estar.tea;
 
+import java.io.*;
+import java.util.*;
 import java.rmi.*;
 import java.rmi.server.*;
+import ngat.util.logging.*;
 
+/** Default implementation for a Telescope Availability prediction service.
+ * Needs to be able to persist this information between reboots incase it
+ * does not get updated in the intervening period.
+ */
 public class DefaultTelescopeAvailabilityPredictor extends UnicastRemoteObject 
     implements  TelescopeAvailabilityPredictor {
 
+    /** The current prediction.*/
     private double available = 1.0;
 
-    public DefaultTelescopeAvailabilityPredictor() throws RemoteException {
-	super();	
+    /** Where we store the TAP data for persistance.*/
+    private File tapDataFile;
+
+    /** Logging.*/
+    private Logger logger;
+
+    /** Create a DefaultTelescopeAvailabilityPredictor using the supplied persistance file.*/
+    public DefaultTelescopeAvailabilityPredictor(File tapDataFile) throws RemoteException {
+	super();
+	this.tapDataFile = tapDataFile;
+	logger = LogManager.getLogger("TRACE");
     }
 
     /** Set the availability prediction for the specified period.
@@ -20,6 +37,26 @@ public class DefaultTelescopeAvailabilityPredictor extends UnicastRemoteObject
     */
     public void setAvailabilityPrediction(long periodStart, long periodEnd, double prediction) throws RemoteException {
 	this.available = prediction;
+
+	long duration = periodEnd - periodStart;
+	logger.log(1, "Received TAP update: For "+
+		   TelescopeEmbeddedAgent.nf.format(duration/360000)+" Hours, starting "+
+		   TelescopeEmbeddedAgent.iso8601.format(new Date(periodStart))+
+		   " Predicted availability = "+TelescopeEmbeddedAgent.nf.format(prediction));
+	
+	// save this info..
+	Properties tap = new Properties();
+	tap.setProperty("from", TelescopeEmbeddedAgent.iso8601.format(new Date(periodStart)));
+	tap.setProperty("to",   TelescopeEmbeddedAgent.iso8601.format(new Date(periodEnd)));
+	tap.setProperty("availability", TelescopeEmbeddedAgent.nf.format(prediction));
+
+	try {
+	    FileOutputStream fout = new FileOutputStream(tapDataFile);
+	    tap.store(fout, "Prediction set on: "+TelescopeEmbeddedAgent.iso8601.format(new Date()));
+	    fout.close();
+	} catch (Exception e) {
+	    throw new RemoteException("Problem persisting TAP update",e);
+	}
     }
 
     /** @return the predicted availability of the telescope over the specified period.
