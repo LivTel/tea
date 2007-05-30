4783d5bfb381e0be91b7a86e8840002e07d90043
Steve Fraser <snf@example.com>
2007-05-30 09:45:05 +0000

    added logger

--- tea/java/org/estar/tea/DefaultEmbeddedAgentRequestHandler.java
+++ tea/java/org/estar/tea/DefaultEmbeddedAgentRequestHandler.java
@@ -3,6 +3,7 @@ package org.estar.tea;
 import java.rmi.*;
 import java.rmi.server.*;
 
+import ngat.util.logging.*;
 import org.estar.rtml.*;
 
 public class DefaultEmbeddedAgentRequestHandler extends UnicastRemoteObject 
@@ -10,29 +11,35 @@ public class DefaultEmbeddedAgentRequestHandler extends UnicastRemoteObject
 
     TelescopeEmbeddedAgent tea;
 
+    Logger logger;
+
+
     /** Create a DefaultEmbeddedAgentRequestHandler for the TEA.*/
     public DefaultEmbeddedAgentRequestHandler(TelescopeEmbeddedAgent tea) throws RemoteException {
 	this.tea = tea;
+	logger = LogManager.getLogger("TRACE");
     } 
 
     /** Handle a scoring request.*/
     public RTMLDocument handleScore(RTMLDocument doc) throws RemoteException {
 	RTMLDocument reply = null;
 	try {
-	    ScoreDocumentHandler sdh = new ScoreDocumentHandler(tea);
+	    ScoreDocumentHandler sdh = new ScoreDocumentHandler(tea);	
 	    reply = sdh.handleScore(doc);
-	} catch (Exception e) { 
+	    logger.log(1, "ScoreDocHandler returned doc: "+reply);
+	} catch (Exception e) { 	  
 	    throw new RemoteException("Exception while handling score: "+e);
 	}
 	return reply;
     }
-
+    
     /** Handle a request request.*/
     public RTMLDocument handleRequest(RTMLDocument doc) throws RemoteException {
 	RTMLDocument reply = null;
 	try {
 	    RequestDocumentHandler rdh = new RequestDocumentHandler(tea);
 	    reply = rdh.handleRequest(doc);
+	    logger.log(1, "RequestDocHandler returned doc: "+reply);
 	} catch (Exception e) {
 	    throw new RemoteException("Exception while handling request: "+e);
 	}
