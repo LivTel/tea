910cf083704f2fb4c9a320ce70352882d16f8fdb
Steve Fraser <snf@example.com>
2008-03-20 12:21:36 +0000

    added test harness stuff

--- tea/java/org/estar/tea/DefaultEmbeddedAgentRequestHandler.java
+++ tea/java/org/estar/tea/DefaultEmbeddedAgentRequestHandler.java
@@ -7,7 +7,7 @@ import ngat.util.logging.*;
 import org.estar.rtml.*;
 
 public class DefaultEmbeddedAgentRequestHandler extends UnicastRemoteObject 
-    implements EmbeddedAgentRequestHandler {
+    implements EmbeddedAgentRequestHandler, EmbeddedAgentTestHarness {
 
     TelescopeEmbeddedAgent tea;
 
@@ -57,5 +57,48 @@ public class DefaultEmbeddedAgentRequestHandler extends UnicastRemoteObject
 	}
 	return reply;
     }
+
+    /** Request to return an RTML <i>update</i> document via the normal NodeAgentAsynchronousResponseHandler mechanism.
+     * @param doc The source document.
+     * @param howlong How long to wait before doing that which needs doing (ms).     
+     */
+    public void testUpdateCallback(RTMLDocument doc, long howlong) throws RemoteException {
+	
+	RTMLIntelligentAgent userAgent = doc.getIntelligentAgent();
+	String agid = "Unknown";
+	String host = "Unknown";
+	int    port = -1;
+	if(userAgent == null) {
+	    traceLog.log(INFO, 1, CLASS, "testUpdateCallback: Warning, User agent was null.");
+	} else {
+	    agid = userAgent.getId();
+	    host = userAgent.getHostname();
+	    port = userAgent.getPort();
+	    logger.log(1, "Testharness: testUpdateCallback: Sending update to: "+agid+"@ "+host+":"+port+" in "+howlong+" msec");
+	}
+
+	doc.setType("update");
+	
+	final TelescopeEmbeddedAgent mytea = tea;
+	final long myhowlong = howlong;
+	final String myagent = agid+"@ "+host+":"+port;
+	Runnable r = new Runnable() {
+		public void run() {
+		    try {	
+			try {Thread.sleep(myhowlong);} catch (InterruptedException ix) {}
+			mytea.sendDocumentToIA(doc);
+			logger.log(1, "Testharness: testUpdateCallback: Sent update to: "+myagent);
+		    } catch (Exception e) {
+			logger.log(1, "An error occurred during TestHarness callback test: "+e);
+			e.printStackTrace();
+		    }
+		}
+	    };
+	
+	Thread thread = new Thread(r);
+	thread.start();
+	
+    }
+
 
 }
